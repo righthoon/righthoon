82c1aab41a3013a78bb94833161d6f34e4e9f7d5
righthoon <88523224+righthoon@users.noreply.github.com>
2021-08-06 12:59:07 +0900

    Delete py4e directory

--- py4e/second.py
+++
@@ -1,4 +0,0 @@
-#floor change
-inp=input('Europe floor?')
-usf=int(inp)+1
-print('Us floor',usf)
--- py4e/week3-q1.py
+++
@@ -1,9 +0,0 @@
-def gugudan(number):
-    i=1
-    print(number,'단')
-    while number*i<=50:
-        print(number,'X',i,'=',number*i)
-        i=i+2
-
-number=int(input('몇 단?:'))
-gugudan(number)
